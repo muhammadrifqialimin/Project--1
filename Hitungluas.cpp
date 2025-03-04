4e4ba2c39b62590250e133d66d56346a794bdc17
Muhammad Rifqi Alimin <rifqi.alimin.ft23@mail.umy.ac.id>
2025-03-04 19:40:10 +0700

    first commit

---
+++ Hitungluas.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+float Luas, Panjang, Lebar;
+
+void InputData()
+{
+    cout << "Masukkan Panjang : ";
+    cin >> Panjang;
+    cout << "Masukkan Lebar : ";
+    cin >> Lebar;
+}
+float HitungLuas()
+{
+    return Panjang * Lebar;
+}
+void TampilData()
+{
+    cout << "Luas Persegi Panjang : " << HitungLuas();
+}
+
+int main()
+{
+    InputData();
+    TampilData();
+    
+}
